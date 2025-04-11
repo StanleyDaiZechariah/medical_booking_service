f5ccaf6513b58604612cb6a0c8192ad41b4c8c68
StanleyDaiZechariah <2606258311@qq.com>
2025-04-11 16:23:56 +0800

    首页医院组建的拆分与静态搭建

---
+++ src/pages/home/conponent/Region.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="region">
+        <div class="content">
+            <div class="left">地区: </div>
+            <ul>
+                <li class="active">首页</li>
+                <li v-for="item in 40" :key="item">首页</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup lang='ts' name="Region">
+
+</script>
+
+<style scoped lang='scss'>
+    .region {
+        color: #7f7f7f;
+        margin-top: 10px;
+
+        .content {
+            display: flex;
+
+            .left {
+                margin-right: 10px;
+                width: 57px;
+            }
+
+            ul {
+                display: flex;
+                flex-wrap: wrap;
+
+                li {
+                    margin-right: 10px;
+                    margin-bottom: 10px;
+
+                    &.active {
+                        color: #55a6fe;
+                    }
+                }
+                li:hover {
+                    color:#55a6fe;
+                    cursor: pointer;
+                }
+            }
+        }
+    }
+</style>
