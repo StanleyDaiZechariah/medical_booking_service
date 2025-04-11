fbd5eccfc6709076b63e80cb3fc4517a5e6daf61
StanleyDaiZechariah <2606258311@qq.com>
2025-04-11 16:26:43 +0800

    首页等级与地区组件拆分与静态搭建

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,13 +4,28 @@
         <Carousel />
         <!-- 搜索框 -->
         <Search />
+        <!-- 底部展示医院的信息 -->
+         <el-row>
+            <el-col :span="20">
+                <!-- 等级子组件 -->
+                <Level />
+                <!-- 地区 -->
+                 <Region />
+            </el-col>
+            <el-col :span="4">456</el-col>
+         </el-row>
     </div>
 </template>
 
 <script setup lang='ts'>
 // 引入首页轮播图组件
 import Carousel from "@/pages/home/conponent/Carousel.vue";
+// 引入首页搜索框组件
 import Search from "./conponent/Search.vue";
+// 引入首页等级的组件
+import Level from "./conponent/Level.vue";
+// 引入首页地区的组件
+import Region from "./conponent/Region.vue";
 
 </script>
 
